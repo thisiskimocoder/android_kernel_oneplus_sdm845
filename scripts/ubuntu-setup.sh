2b76d40f026f21cd98cd3ac08908773553540ba0
kimocoder <christian@aircrack-ng.org>
2024-08-24 23:42:39 +0200

    Add build and import/upstream tools

---
+++ scripts/ubuntu-setup.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+#
+# Script to set up an Ubuntu 22.04+ server
+# (with minimum 16GB RAM, 8 threads CPU) for android ROM compiling
+#
+# Sudo access is mandatory to run this script
+#
+# IMPORTANT NOTICE: This script sets my personal git config, update
+# it with your details before you run this script!
+#
+# Usage:
+#	./ubuntu-setup.sh
+#
+
+echo -e "Installing and updating APT packages...\n"
+sudo apt update -qq
+sudo apt install -y -qq git-core gnupg flex bc bison build-essential zip curl zlib1g-dev gcc-multilib \
+                        g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev jq \
+                        lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig imagemagick \
+                        python3 python3-pip python3-dev python-is-python3 schedtool ccache libtinfo5 \
+                        libncurses5 lzop tmux libssl-dev neofetch patchelf apktool dos2unix git-lfs default-jdk \
+                        libxml-simple-perl rsync nano ripgrep nload xmlstarlet binwalk tmate
+sudo apt autoremove -y -qq
+sudo apt purge snapd -y -qq
+echo -e "\nDone."
+
+echo -e "Installing and enabling docker... \n"
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo -e "Done."
+
+echo -e "\nInstalling git-repo..."
+wget -q https://storage.googleapis.com/git-repo-downloads/repo
+chmod a+x repo
+sudo install repo /usr/local/bin/repo
+rm repo
+echo -e "Done."
+
+echo -e "\nInstalling gh-cli..."
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+echo -e "Done."
+
+echo -e "\nInstalling apktool and JADX..."
+mkdir -p bin
+wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.9.0.jar -O bin/apktool.jar
+echo 'alias apktool="java -jar $HOME/bin/apktool.jar"' >> .bashrc
+
+wget -q https://github.com/skylot/jadx/releases/download/v1.4.7/jadx-1.4.7.zip -O jadx.zip
+unzip -qq jadx.zip -d jadx
+rm jadx.zip
+echo 'export PATH="$HOME/jadx/bin:$PATH"' >> .bashrc
+echo -e "Done."
+
+cat <<'EOF' >> .bashrc
+# Super-fast repo sync
+repofastsync() { time schedtool -B -e ionice -n 0 `which repo` sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --retry-fetches=5 -j$(nproc --all) "$@"; }
+
+# List lib dependencies of any lib/bin
+list_blob_deps() { readelf -d $1 | grep "\(NEEDED\)" | sed -r "s/.*\[(.*)\]/\1/"; }
+export TZ='Asia/Kolkata'
+EOF
+
+# Unlimited history file
+sed -i 's/HISTSIZE=.*/HISTSIZE=-1/g' .bashrc
+sed -i 's/HISTFILESIZE=.*/HISTFILESIZE=-1/g' .bashrc
+
+echo -e "Done."
+
+# Increase tmux scrollback buffer size
+echo "set-option -g history-limit 6000" >> .tmux.conf
+
+# Increase maximum ccache size
+ccache -M 100G
+
+# Configure git
+echo -e "\nSetting up Git..."
+
+if [[ $USER == "karan" ]]; then
+  git config --global user.email "karan@pixelos.net"
+  git config --global user.name "Karan Parashar"
+fi
+
+git config --global alias.cp 'cherry-pick'
+git config --global alias.c 'commit'
+git config --global alias.f 'fetch'
+git config --global alias.m 'merge'
+git config --global alias.rb 'rebase'
+git config --global alias.rs 'reset'
+git config --global alias.ck 'checkout'
+git config --global credential.helper 'cache --timeout=99999999'
+git config --global core.editor "nano"
+echo "Done."
+
+# Done!
+echo -e "\nALL DONE. Now sync sauces & start baking!"
+echo -e "Please relogin or run \`source ~/.bashrc && source ~/.profile\` for environment changes to take effect."
